523c179604e8f09b7d76dfcd612793d03e9da997
Marcio <marciosagaz@yahoo.com.br>
2017-04-05 23:24:02 -0300

    New configuration was added

--- configuration.lua
+++ configuration.lua
@@ -1,15 +1,15 @@
 local configuration = {
   SIZE=3,
   HEURISTIC = {
-    MANHATTAN = false,
-    OUT_OF_PLACE = true,
+    MANHATTAN = true,
+    OUT_OF_PLACE = false,
   },
 
   -- INITIAL={ 2, 8, 3, 1, 6, 4, 7, 9, 5 },
   -- FINAL={1, 2, 3, 8, 9, 4, 7, 6, 5 },
 
-  INITIAL={ 2, 3, 5, 6, 9, 4, 7, 1, 8 },
-  FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9 },
+  -- INITIAL={ 2, 3, 5, 6, 9, 4, 7, 1, 8 },
+  -- FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9 },
 
   -- INITIAL={6, 7, 1, 4, 5, 2, 3, 9, 8 },
   -- FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9 },
@@ -19,6 +19,12 @@ local configuration = {
 
   -- INITIAL={ 9, 8, 7, 6, 5, 4, 3, 2, 1 },
   -- FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9 },
+
+  INITIAL={ 2, 3, 1, 9, 4, 6, 7, 5, 8 },
+  FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9 },
+
+  -- INITIAL={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 12,13,14,11,15 },
+  -- FINAL={1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16 },
 }
 
 return configuration
--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ local game = require "engine"
 local function log(map, cost, level, visited, novisited)
   print( 'map', map, 'cost', cost, 'level', level, 'visited', visited, 'novisited', novisited)
 end
+
 local timestart = os.time()
 local result = game.start(log)
 print('time:',os.time()-timestart)
@@ -12,13 +13,13 @@ local steps = {}
 if result.success then
   local parentId = result.node.state.id
   repeat
-    table.insert(steps, 1, (string.gsub(string.gsub(parentId,("(%d%d%d)"):rep(3),"|%1|\n|%2|\n|%3|\n"),'9',' ')) )
+    table.insert(steps, 1, (string.gsub(string.gsub(parentId,("(%d%d%d)"):rep(3),"_____\n|%1|\n|%2|\n|%3|\n¨¨¨¨¨\n"),'9',' ')) )
     parentId = result.steps[parentId].parentId
   until not parentId
 end
 
-for _, content in ipairs(steps) do
-  print("_____",_)
+for move, content in ipairs(steps) do
+  print('move',move)
   print(content)
   print("Enter to next move.")
   io.read()
